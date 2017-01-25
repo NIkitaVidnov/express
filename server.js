47f159d1deebbc9e68071af7eeeef608b74d3c20
Nikita Vidnov <vidnov@outlook.com>
2017-01-25 04:58:43 +0300

    Первый запуск сервера

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+ var app = express();
+
+app.get('/',function(reg, res){//Что будет происходить при  входе через браузер сервер (localhost:3012)
+    res.send('Hello API');
+})
+
+app.listen(3010, function(){ //Назначаем порт
+    console.log('API app started'); // Что выведется в консоли при запуске сервера
+})
