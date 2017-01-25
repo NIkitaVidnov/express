f05982856f4e3da8e4d0d97c87543edd2763ca3b
Nikita Vidnov <vidnov@outlook.com>
2017-01-25 05:16:48 +0300

    Добавление массива объектов(Артисты)

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express');
  var app = express();
+ var artist =[
+     {
+         id:1,
+         name:'Metallica'
+     },
+     {
+         id :2,
+         name:'tech9'
+     },
+     {
+         id:3,
+         name:'wekeend'
+     }
+ ];
 
 app.get('/',function(reg, res){//Что будет происходить при  входе через браузер сервер (localhost:3012)
     res.send('Hello API');
+})
+
+app.get('/artist',function(reg,res){
+    res.send(artist);
 })
 
 app.listen(3010, function(){ //Назначаем порт
